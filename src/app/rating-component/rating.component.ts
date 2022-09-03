1257ae2db4184943ac48afe5ae7cc5074348ede9
Quentin Serda <quentinserda@gmail.com>
2022-09-03 13:57:51 +0200

    try to condition the button

--- src/app/rating-component/rating.component.ts
+++ src/app/rating-component/rating.component.ts
@@ -9,7 +9,8 @@ import {faStar} from "@fortawesome/free-solid-svg-icons";
 export class RatingComponent {
   display = false;
   faStar = faStar;
-  radios = [false, false, false, false, false];
+  radios = ['1', '2', '3', '4', '5'];
+  isChecked = false;
 
   constructor() {
 
@@ -17,9 +18,5 @@ export class RatingComponent {
 
   submitResult() {
     this.display = !this.display;
-  }
-
-  allChecked() {
-    return this.radios.indexOf(false) === -1;
   }
 }
