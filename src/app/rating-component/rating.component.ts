ff5931eb8c4d73a325f858294e2f2e3e8e3dac16
Quentin Serda <quentinserda@gmail.com>
2022-09-04 23:43:24 +0200

    try to condition the button

--- src/app/rating-component/rating.component.ts
+++ src/app/rating-component/rating.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {faStar} from "@fortawesome/free-solid-svg-icons";
+import {formatNumber} from "@angular/common";
 
 @Component({
   selector: 'app-rating-component',
@@ -9,14 +10,17 @@ import {faStar} from "@fortawesome/free-solid-svg-icons";
 export class RatingComponent {
   display = false;
   faStar = faStar;
-  radios = ['1', '2', '3', '4', '5'];
+  radios = [1, 2, 3, 4, 5];
   isChecked = false;
+  focus = false;
 
   constructor() {
-
   }
 
   submitResult() {
     this.display = !this.display;
+  }
+  onFocus() {
+    this.focus = true;
   }
 }
